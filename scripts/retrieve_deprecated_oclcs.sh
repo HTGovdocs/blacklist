27ac9e97ff15bb5382d2aec668356874712d36df
Josh Steverman <jstever@umich.edu>
2018-08-30 10:22:25 -0400

    copy list of filtered oclcs to /htdata

--- scripts/retrieve_deprecated_oclcs.sh
+++ scripts/retrieve_deprecated_oclcs.sh
@@ -1,4 +1,8 @@
 export PATH="$HOME/.rbenv/bin:$PATH"
 export PATH="$HOME/bin:$PATH"
 
+# copy to /htdata
+D=$(date +\%Y-\%m-\%d)
+cp data/oclcs_removed_from_registry.txt /htdata/govdocs/feddocs_oclc_filter/oclcs_removed_from_registry_$D.txt
+
 git commit data/oclcs_removed_from_registry.txt -m 'OCLC update' --author="Josh Steverman <jstever@umich.edu>"
