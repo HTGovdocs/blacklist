f5bfe2438d447a96f903b8a988f202952a4b2bbc
Josh Steverman <jstever@umich.edu>
2022-03-16 18:16:51 -0400

    Relative path for authory and rejected lists

--- lib/filter/authority_list.rb
+++ lib/filter/authority_list.rb
@@ -5,6 +5,6 @@ require "pry"
 class AuthorityList
   class << self; attr_accessor :lccns; end
 
-  self.lccns = File.open("data/authority_list.tsv").readlines
+  self.lccns = File.open(__dir__ + "/../../data/authority_list.tsv").readlines
     .each { |line| line.chomp.split("\t").first }
 end
--- lib/filter/rejected_list.rb
+++ lib/filter/rejected_list.rb
@@ -3,6 +3,6 @@
 class RejectedList
   class << self; attr_accessor :oclcs; end
 
-  self.oclcs = File.open("data/rejected_oclc_nums.txt").readlines
+  self.oclcs = File.open(__dir__ + "/../../data/rejected_oclc_nums.txt").readlines
     .each { |o| o.sub(/^0+/, "") }.reject(&:empty?).map(&:to_i).to_set
 end
