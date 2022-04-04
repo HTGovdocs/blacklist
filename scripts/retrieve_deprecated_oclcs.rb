e5b95778847aa67882a5277e75f38bb23dcce152
Josh Steverman <jstever@umich.edu>
2022-04-04 14:14:44 -0400

    removed extraneous fetching operations

--- scripts/retrieve_deprecated_oclcs.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "filter/rejected_list"
-list = File.open(ARGV.shift, "w")
-RejectedList.oclcs.each { |o| list.puts o }
-list.close
-
-`scripts/retrieve_deprecated_oclcs.sh`
